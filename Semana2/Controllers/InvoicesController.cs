ccb3830be460e27c6f7e03d169659b775ad5fbec
Luis Rodriguez <developer0606@gmail.com>
2019-11-02 11:25:45 -0400

    first commit

---
+++ Semana2/Controllers/InvoicesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Semana2.Models;
+
+namespace semana2.Controllers
+{
+    public class InvoicesController : Controller
+    {
+        private Model1 db = new Model1();
+
+        // GET: Invoices
+        public ActionResult Index()
+        {
+            return View(db.Invoices.ToList());
+        }
+
+        // GET: Invoices/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Invoice invoice = db.Invoices.Find(id);
+            if (invoice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(invoice);
+        }
+
+        // GET: Invoices/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Invoices/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CustomerName,Salesperson,OrderID,ShipperName,ProductID,ProductName,UnitPrice,Quantity,Discount,ShipName,ShipAddress,ShipCity,ShipRegion,ShipPostalCode,ShipCountry,CustomerID,Address,City,Region,PostalCode,Country,OrderDate,RequiredDate,ShippedDate,ExtendedPrice,Freight")] Invoice invoice)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Invoices.Add(invoice);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(invoice);
+        }
+
+        // GET: Invoices/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Invoice invoice = db.Invoices.Find(id);
+            if (invoice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(invoice);
+        }
+
+        // POST: Invoices/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CustomerName,Salesperson,OrderID,ShipperName,ProductID,ProductName,UnitPrice,Quantity,Discount,ShipName,ShipAddress,ShipCity,ShipRegion,ShipPostalCode,ShipCountry,CustomerID,Address,City,Region,PostalCode,Country,OrderDate,RequiredDate,ShippedDate,ExtendedPrice,Freight")] Invoice invoice)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(invoice).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(invoice);
+        }
+
+        // GET: Invoices/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Invoice invoice = db.Invoices.Find(id);
+            if (invoice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(invoice);
+        }
+
+        // POST: Invoices/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Invoice invoice = db.Invoices.Find(id);
+            db.Invoices.Remove(invoice);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
